4ec56cffff05f4ae09210368bc64822636274d00
Margarida Cosme <44810426+margaridacosme@users.noreply.github.com>
2019-10-16 10:31:31 +0100

    Update MyTorus.js

--- MyTorus.js
+++ MyTorus.js
@@ -44,14 +44,5 @@ class MyTorus extends CGFobject{
 		this.initGLBuffers();
 	};
 
-	updateTexCoords(s,t){
-		this.texCoords = this.defaultTexCoords.slice();
-
-		 for(var i = 0; i < this.texCoords.length; i+=2){
-			this.texCoords[i] /= s;
-			this.texCoords[i+1] /= t;
-		}
-
-		 this.updateTexCoordsGLBuffers();
-	};
+	updateTexCoords(s,t){};
 };
