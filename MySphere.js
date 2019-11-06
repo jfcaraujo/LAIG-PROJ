a490b04592ba628b1497f640831f44f91cbc4cd9
Margarida Cosme <44810426+margaridacosme@users.noreply.github.com>
2019-11-06 08:32:17 +0000

    Add files via upload

--- MySphere.js
+++ MySphere.js
@@ -26,13 +26,19 @@ class MySphere extends CGFobject {
 		for (var i = 0; i <= this.stacks; i++) {
 			for (var j = 0; j <= this.slices; j++) {
 
-				this.vertices.push((this.radius * Math.cos(l * j)) * Math.cos(s * i), (this.radius * Math.cos(l * j)) * Math.sin(s * i), this.radius * Math.sin(s * j));
+				this.vertices.push(
+					(this.radius * Math.cos(s * j)) * Math.sin(l * i),
+					this.radius * Math.cos(l * i), 
+					(this.radius * Math.sin(s * j) * Math.sin(l * i)));
 
-				this.normals.push((this.radius * Math.cos(l * j)) * Math.cos(s * i), (this.radius * Math.cos(l * j)) * Math.sin(s * i), this.radius * Math.sin(s * j));
+				this.normals.push(
+					(this.radius * Math.cos(s * j)) * Math.sin(l * i),
+					this.radius * Math.cos(l * i), 
+					(this.radius * Math.sin(s * j) * Math.sin(l * i)));
 
 				if (i != this.stacks && j != this.slices) {
-					this.indices.push(j * (this.slices + 1) + i, j * (this.slices + 1) + i + this.slices + 1, j * (this.slices + 1) + i + this.slices + 2);
-					this.indices.push(j * (this.slices + 1) + i, j * (this.slices + 1) + i + this.slices + 2, j * (this.slices + 1) + i + 1);
+					this.indices.push(i * (this.slices + 1) + j, i * (this.slices + 1) + j + this.slices + 2, i * (this.slices + 1) + j + this.slices + 1);
+					this.indices.push(i * (this.slices + 1) + j, i * (this.slices + 1) + j + 1, i * (this.slices + 1) + j + 2 + this.slices);
 				}
 
 				this.texCoords.push(1 - i * (1 / this.stacks), 1 - j * (1 / this.slices));
